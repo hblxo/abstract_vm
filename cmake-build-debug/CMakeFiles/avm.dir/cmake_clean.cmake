cb2ded2bcef4609f019719c324f7187dce0d12b2
hbouchet <hbouchet@student.42.fr>
2019-10-02 13:20:26 +0200

    Add cmake config

---
+++ cmake-build-debug/CMakeFiles/avm.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/avm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/avm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
