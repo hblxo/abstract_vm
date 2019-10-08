3436a6b781601abf1df4356cba9a45875380590a
hbouchet <hbouchet@student.42.fr>
2019-10-08 18:22:11 +0200

    Clean

--- srcs/LexerClass.cpp
+++ srcs/LexerClass.cpp
@@ -20,7 +20,6 @@
 
 Lexer::Lexer(int argc, char **argv)
 {
-//TODO : check EOF
     std::ifstream file;
     std::string filename;
 
@@ -40,7 +39,6 @@ Lexer::Lexer(int argc, char **argv)
     	Lexer::readInput(std::cin);
 		Lexer::run();
 	}
-    return;
 }
 
 void	Lexer::readInput(std::istream &input)
@@ -64,8 +62,7 @@ void 	Lexer::readFile(std::istream &file)
 	std::list<std::string>::iterator	it;
 	for (it = _input.begin(); it != _input.end() && (*it) != "exit" ; it++)
 		;
-//	if (it == _input.end())
-			if ((std::find(_input.begin(), _input.end(), "exit") == _input.end()))
+	if ((std::find(_input.begin(), _input.end(), "exit") == _input.end()))
 		throw NoExitInstructionException();
 }
 
@@ -78,22 +75,10 @@ void Lexer::run()
     for (ite = _input.begin(); ite != _input.end() && (*ite) != "exit" && (*ite) != ";;" ; ite++)
 	{
 		if ((*ite)[0] == ';')
-    		;//TODO : traiter commentaire debut de ligne
-        else if ((*ite).size() != 0 && (*ite)[0] != '\n')
+    		;
+		else if ((*ite).size() != 0 && (*ite)[0] != '\n')
             defineLexerInstruct((*ite));
 	}
-//    getline(file, cmd);
-//    while (cmd != ";;" && cmd != "exit")
-//    {
-//    	if (cmd[0] == ';')
-//    		;//TODO : traiter commentaire debut de ligne
-//        else if (cmd.size() != 0 && cmd[0] != '\n')
-//            defineLexerInstruct(cmd);
-//        cmd.clear();
-//    	if (!getline(file, cmd))
-//    		if ((file.eof()))
-//				throw NoExitInstructionException();
-//    }
 }
 
 std::string	Lexer::parseLine(std::string line)
@@ -101,9 +86,6 @@ std::string	Lexer::parseLine(std::string line)
 	std::size_t commentPos;
 
 	commentPos = line.find(";");
-
-//	std::string comment = line.substr(commentPos + 1);
-	//TODO : traiter commentaire en fin de ligne
 	std::string value = line.substr(0, commentPos);
 	return value;
 }
@@ -112,20 +94,16 @@ void    Lexer::defineLexerInstruct(std::string string)
 {
     std::list<Matcher*>::iterator ite;
     std::size_t pos;
-//	auto calc = new Calculator();
 
     std::string line = Lexer::parseLine(string);
     pos = line.find(" ");
 	std::string value = line.substr(0, pos);
-      //TODO : parser commentaire en fin d'instruction
 
     for(ite = _matchList->begin(); ite != _matchList->end(); ite++)
     {
         if ((*ite)->matchSearch(value))
         {
             if (pos != std::string::npos)
-//            && std::regex_match(line,
-//            		std::regex("^(\\s*)?(push|assert)(\\s+)(((int8|int16|int32)[(]-?[0-9]+[)])|((float|double)[(]-?[0-9]+(.?[0-9]+)?[)]))(\\s*)(;.*)?$")))
                 _calc.doOperation((*ite)->getType(), line.substr(pos + 1));
             else
                 _calc.doOperation((*ite)->getType());
@@ -160,7 +138,6 @@ Lexer::Lexer()
 
 Lexer::Lexer(Lexer const & src) 
 {
-    //Do whatever needs to be done
     *this = src;
 }
 
@@ -171,7 +148,6 @@ Lexer::~Lexer()
 
 Lexer &	Lexer::operator=(Lexer const & rhs)
 {
-    //Do whatever needs to be done
     if (this != &rhs)
         *this = rhs;
     return *this;
